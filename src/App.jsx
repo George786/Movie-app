d47e74911285ac42f73111728ea70aefbcd87a84
George <hndsmby123@gmail.com>
2025-09-15 15:11:13 +0530

    Authentication in API

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,14 @@ const API_BASE_URL = 'https://api.themoviedb.org/3';
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
-// const API_OPTIONS = {
-//     method: 'GET',
-//     headers: {
-//         accept: 'application/json',
-//         Authorization: `Bearer ${API_KEY}`
-//     }
-// }
 const API_OPTIONS = {
     method: 'GET',
     headers: {
-        accept: 'application/json'
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
     }
 }
+
 
 
 const App = () => {
@@ -48,12 +43,9 @@ const App = () => {
         setErrorMessage('');
 
         try {
-            // const endpoint = query
-            //     ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-            //     : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
             const endpoint = query
-                ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}&api_key=${API_KEY}`
-                : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+                ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
             const response = await fetch(endpoint, API_OPTIONS);
 
